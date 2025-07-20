531208b1c2e371e4dc819f1feba53c6460813e16
PAWAN YADAV  (AI Engineer) <129971371+2000pawan@users.noreply.github.com>
2025-07-20 08:38:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -37,4 +37,5 @@ def static_proxy(path):
     return send_from_directory("../frontend", path)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
